4ef451016dc8d946cd09cfdb2958f9380825abcf
ReyazShaik <45187103+ReyazShaik@users.noreply.github.com>
2025-01-25 20:05:08 +0530

    Add files via upload

---
+++ jenkins.sh
@@ -0,0 +1,17 @@
+#STEP-1: Installing Git and Maven
+yum install git maven -y
+
+#STEP-2: Repo Information (jenkins.io --> download -- > redhat)
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+#STEP-3: Download Java 17 and Jenkins
+sudo yum install java-17-amazon-corretto -y
+yum install jenkins -y
+
+#STEP-4: Start and check the JENKINS Status
+systemctl start jenkins.service
+systemctl status jenkins.service
+
+#STEP-5: Auto-Start Jenkins
+chkconfig jenkins on
