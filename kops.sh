9b9d094ed779345085d6d165d5787f618c2e7f4b
Reyaz Shaik <45187103+ReyazShaik@users.noreply.github.com>
2025-02-27 12:52:09 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -1,14 +1,20 @@
+# Add Admin IAM ROLE to EC2
+#vim .bashrc
+#export PATH=$PATH:/usr/local/bin/
+#source .bashrc
+
+
 #! /bin/bash
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
 chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
-echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
-source ~/.bashrc
+
 aws s3api create-bucket --bucket reyaz-kops-testbkt123.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
 aws s3api put-bucket-versioning --bucket reyaz-kops-testbkt123.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://reyaz-kops-testbkt123.k8s.local
 kops create cluster --name reyaz.k8s.local --zones ap-south-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
 kops update cluster --name reyaz.k8s.local --yes --admin
-kops validate cluster --wait 10m
+
+#kops validate cluster --wait 10m
