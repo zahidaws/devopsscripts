5f9815bd0781b5d1e7fe9f007f9dc424b4a5d0a4
Reyaz Shaik <45187103+ReyazShaik@users.noreply.github.com>
2025-03-13 15:31:38 +0530

    Create terraformAL2.sh

---
+++ terraformAL2.sh
@@ -0,0 +1,5 @@
+sudo yum update -y
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum install -y terraform
+terraform version
