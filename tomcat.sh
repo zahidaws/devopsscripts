b253be556b8368fdf9a085c0209806234dc387c4
Reyaz Shaik <45187103+ReyazShaik@users.noreply.github.com>
2025-06-21 16:34:39 +0530

    Update tomcat.sh with latest version

--- tomcat.sh
+++ tomcat.sh
@@ -1,11 +1,11 @@
 amazon-linux-extras install java-openjdk11 -y
-wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.39/bin/apache-tomcat-10.1.39.tar.gz
-tar -zxvf apache-tomcat-10.1.39.tar.gz
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-10.1.39/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-10.1.39/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="root123556" roles="manager-gui, manager-script"/>' apache-tomcat-10.1.39/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-10.1.39/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-10.1.39/conf/tomcat-users.xml
-sed -i '21d' apache-tomcat-10.1.39/webapps/manager/META-INF/context.xml
-sed -i '22d' apache-tomcat-10.1.39/webapps/manager/META-INF/context.xml
-sh apache-tomcat-10.1.39/bin/startup.sh
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.42/bin/apache-tomcat-10.1.42.tar.gz
+tar -zxvf apache-tomcat-10.1.42.tar.gz
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-10.1.42/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-10.1.42/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="root123" roles="manager-gui, manager-script"/>' apache-tomcat-10.1.42/conf/tomcat-users.xml
+sed -i '59  a\</tomcat-users>' apache-tomcat-10.1.42/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-10.1.42/conf/tomcat-users.xml
+sed -i '21d' apache-tomcat-10.1.42/webapps/manager/META-INF/context.xml
+sed -i '22d' apache-tomcat-10.1.42/webapps/manager/META-INF/context.xml
+sh apache-tomcat-10.1.42/bin/startup.sh
