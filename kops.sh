d29dd1dff8687988e54e1d3c33501310d6d2779e
Reyaz Shaik <45187103+ReyazShaik@users.noreply.github.com>
2025-07-13 09:32:34 +0530

    updated kops.sh

--- kops.sh
+++ kops.sh
@@ -6,15 +6,14 @@
 
 #! /bin/bash
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
+wget https://github.com/kubernetes/kops/releases/download/v1.32.0/kops-linux-amd64
 chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
-
-aws s3api create-bucket --bucket reyaz-kops-testbkt123.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
-aws s3api put-bucket-versioning --bucket reyaz-kops-testbkt123.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://reyaz-kops-testbkt123.k8s.local
-kops create cluster --name reyaz.k8s.local --zones ap-south-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
+aws s3api create-bucket --bucket reyaz-kops-testbkt143.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
+aws s3api put-bucket-versioning --bucket reyaz-kops-testbkt143.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://reyaz-kops-testbkt143.k8s.local
+kops create cluster --name reyaz.k8s.local --zones ap-south-1a --image ami-0f918f7e67a3323f0  --control-plane-count=1 --control-plane-size t2.medium --node-count=2 --node-size t2.micro
 kops update cluster --name reyaz.k8s.local --yes --admin
 
 #kops validate cluster --wait 10m
