6a079ce0f2560aae3c3043688f881237fd045ce9
Reyaz Shaik <45187103+ReyazShaik@users.noreply.github.com>
2025-07-31 00:45:26 +0530

    Update tomcat.sh with AL2023

--- tomcat.sh
+++ tomcat.sh
@@ -1,11 +1,11 @@
-amazon-linux-extras install java-openjdk11 -y
-wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.42/bin/apache-tomcat-10.1.42.tar.gz
-tar -zxvf apache-tomcat-10.1.42.tar.gz
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-10.1.42/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-10.1.42/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="root123" roles="manager-gui, manager-script"/>' apache-tomcat-10.1.42/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-10.1.42/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-10.1.42/conf/tomcat-users.xml
-sed -i '21d' apache-tomcat-10.1.42/webapps/manager/META-INF/context.xml
-sed -i '22d' apache-tomcat-10.1.42/webapps/manager/META-INF/context.xml
-sh apache-tomcat-10.1.42/bin/startup.sh
+dnf install java-17-amazon-corretto -y
+wget https://dlcdn.apache.org/tomcat/tomcat-11/v11.0.9/bin/apache-tomcat-11.0.9.tar.gz
+tar -zxvf apache-tomcat-11.0.9.tar.gz
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-11.0.9/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-11.0.9/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="root123" roles="manager-gui, manager-script"/>' apache-tomcat-11.0.9/conf/tomcat-users.xml
+sed -i '59  a\</tomcat-users>' apache-tomcat-11.0.9/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-11.0.9/conf/tomcat-users.xml
+sed -i '21d' apache-tomcat-11.0.9/webapps/manager/META-INF/context.xml
+sed -i '22d' apache-tomcat-11.0.9/webapps/manager/META-INF/context.xml
+sh apache-tomcat-11.0.9/bin/startup.sh
