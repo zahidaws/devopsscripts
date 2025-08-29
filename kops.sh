b681f710fd6292134d868fe0540183b06eee3042
Reyaz Shaik <45187103+ReyazShaik@users.noreply.github.com>
2025-08-29 19:28:31 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -1,19 +1,52 @@
-# Add Admin IAM ROLE to EC2
-#vim .bashrc
-#export PATH=$PATH:/usr/local/bin/
-#source .bashrc
+Launch Amazon Linux 2023 , t2.micro
+
+Attach a IAM ROLE TE=EC2, Permisions = admin
+
+vi .bashrc
+export PATH=$PATH:/usr/local/bin/
 
 
-#! /bin/bash
+source .bashrc
+
+ssh-keygen
+
+cp /root/.ssh/id_rsa.pub my-keypair.pub
+
+chmod 777 my-keypair.pub
+
+vi kops.sh
+
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 wget https://github.com/kubernetes/kops/releases/download/v1.32.0/kops-linux-amd64
 chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
-aws s3api create-bucket --bucket reyaz-kops-testbkt143.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
-aws s3api put-bucket-versioning --bucket reyaz-kops-testbkt143.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://reyaz-kops-testbkt143.k8s.local
-kops create cluster --name reyaz.k8s.local --zones ap-south-1a --image ami-0f918f7e67a3323f0  --control-plane-count=1 --control-plane-size t2.medium --node-count=2 --node-size t2.micro
+aws s3api create-bucket --bucket reyaz-kops-testbkt1433.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
+aws s3api put-bucket-versioning --bucket reyaz-kops-testbkt1433.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://reyaz-kops-testbkt1433.k8s.local
+kops create cluster --name=reyaz.k8s.local --zones=ap-south-1a --control-plane-count=1 --control-plane-size=t3.medium --node-count=2 --node-size=t3.small --node-volume-size=20 --control-plane-volume-size=20 --ssh-public-key=my-keypair.pub --image=ami-02d26659fd82cf299 --networking=calico
 kops update cluster --name reyaz.k8s.local --yes --admin
 
-#kops validate cluster --wait 10m
+
+wq!
+
+export KOPS_STATE_STORE=s3://reyaz-kops-testbkt1433.k8s.local
+kops validate cluster --wait 10m
+
+
+-- kops get cluster
+
+-- kubectl get nodes/no
+
+-- kubectl get nodes -o wide
+
+Suggestions:
+ * list clusters with: kops get cluster
+ * edit this cluster with: kops edit cluster reyaz.k8s.local
+ * edit your node instance group: kops edit ig --name=reyaz.k8s.local nodes-ap-south-1a
+ * edit your control-plane instance group: kops edit ig --name=reyaz.k8s.local control-plane-ap-south-1a
+
+
+
+
+kops delete cluster --name reyaz.k8s.local --yes
