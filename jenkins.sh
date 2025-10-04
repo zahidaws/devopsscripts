4a7e4bc9c31437a30f05c16b03278edc2e8d7425
Reyaz Shaik <45187103+ReyazShaik@users.noreply.github.com>
2025-10-04 21:24:34 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -8,7 +8,7 @@ sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 #STEP-3: Download Java 17 and Jenkins
 sudo yum install java-17-amazon-corretto -y
 yum install jenkins -y
-
+sudo mount -o remount,size=2G /tmp
 #STEP-4: Start and check the JENKINS Status
 systemctl start jenkins.service
 systemctl status jenkins.service
